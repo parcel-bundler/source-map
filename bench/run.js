52f12c29a174ee27b9ff24882f4840f660d34932
Jasper De Moor <jasperdemoor@gmail.com>
2020-03-06 16:57:21 +0100

    update benchmark

--- bench/run.js
+++ bench/run.js
@@ -37,25 +37,25 @@ let parcelSourceMap = new ParcelSourceMap(mappings).serialize();
 let jsonStringParcelSourceMap = JSON.stringify(new ParcelSourceMap(mappings).serialize());
 
 suite.add("@parcel/source-map#consume", async () => {
-  for (let map of test_maps) {
-    let sm = await ParcelSourceMap.fromRawSourceMap(map);
+  for (let testMap of test_maps) {
+    await ParcelSourceMap.fromRawSourceMap(testMap);
   }
 });
 
 suite.add("@parcel/source-map#consume->serialize->JSON.stringify", async () => {
-  for (let map of test_maps) {
-    let sm = await ParcelSourceMap.fromRawSourceMap(map);
-    JSON.stringify(sm.serialize());
+  for (let testMap of test_maps) {
+    let map = await ParcelSourceMap.fromRawSourceMap(testMap);
+    JSON.stringify(map.serialize());
   }
 });
 
 suite.add(
   "@parcel/source-map#consume->serialize->JSON.stringify->JSON.parse->new SourceMap",
   async () => {
-    for (let map of test_maps) {
-      let sm = await ParcelSourceMap.fromRawSourceMap(map);
-      let parsed = JSON.parse(JSON.stringify(sm.serialize()));
-      sm = new ParcelSourceMap(map);
+    for (let testMap of test_maps) {
+      let map = await ParcelSourceMap.fromRawSourceMap(testMap);
+      let parsed = JSON.parse(JSON.stringify(map.serialize()));
+      map = new ParcelSourceMap(parsed);
     }
   }
 );
@@ -75,30 +75,33 @@ suite.add("@parcel/source-map#combine 1000 maps from JSON", async () => {
 });
 
 suite.add("cpp#consume", async () => {
-  for (let map of test_maps) {
-    let sm = new SourceMap(map.mappings, map.sources, map.names);
+  for (let testMap of test_maps) {
+    let map = new SourceMap(testMap.mappings, testMap.sources, testMap.names);
   }
 });
 
 suite.add("cpp#consume->stringify", async () => {
-  for (let map of test_maps) {
-    let sm = new SourceMap(map.mappings, map.sources, map.names);
-    let s = sm.stringify();
+  for (let testMap of test_maps) {
+    let map = new SourceMap(testMap.mappings, testMap.sources, testMap.names);
+    await map.stringify({
+      file: "index.js.map",
+      sourceRoot: "/"
+    })
   }
 });
 
 suite.add("cpp#consume->toBuffer", async () => {
-  for (let map of test_maps) {
-    let sm = new SourceMap(map.mappings, map.sources, map.names);
-    let buff = sm.toBuffer();
+  for (let testMap of test_maps) {
+    let map = new SourceMap(testMap.mappings, testMap.sources, testMap.names);
+    let buff = map.toBuffer();
   }
 });
 
 suite.add("cpp#consume->toBuffer->fromBuffer", async () => {
-  for (let map of test_maps) {
-    let sm = new SourceMap(map.mappings, map.sources, map.names);
-    let buff = sm.toBuffer();
-    sm = new SourceMap(buff);
+  for (let testMap of test_maps) {
+    let map = new SourceMap(testMap.mappings, testMap.sources, testMap.names);
+    let buff = map.toBuffer();
+    map = new SourceMap(buff);
   }
 });
 
@@ -122,7 +125,10 @@ suite.add("cpp#combine 1000 maps->stringify", async () => {
   for (let i = 0; i < 1000; i++) {
     map.addBufferMappings(sourcemapBuffer, i * 4);
   }
-  map.stringify();
+  await map.stringify({
+    file: "index.js.map",
+    sourceRoot: "/"
+  })
 });
 
 suite.run();
