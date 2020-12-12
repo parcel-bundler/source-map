755041502ddf28ddb19e464935de7832e7768ce9
Devon Govett <devongovett@gmail.com>
2020-12-12 15:07:20 -0500

    Only replace path separators on windows (27% faster)

--- test/utils.test.js
+++ test/utils.test.js
@@ -2,33 +2,37 @@ import assert from 'assert';
 import { normalizePath, relatifyPath } from '../src/utils';
 
 describe('Utilities', () => {
-  it('Relative path - WIN32', async () => {
-    let result = relatifyPath('D:\\test\\sub-dir\\file.js', 'D:\\test\\sub-dir\\');
-    assert.equal(result, './file.js');
+  describe('win32', function() {
+    let platform = process.platform;
+
+    before(() => {
+      Object.defineProperty(process, 'platform', {value: 'win32'});
+    });
+
+    after(() => {
+      Object.defineProperty(process, 'platform', {value: platform});
+    });
+
+    it('Relative path', async () => {
+      let result = relatifyPath('D:\\test\\sub-dir\\file.js', 'D:\\test\\sub-dir\\');
+      assert.equal(result, './file.js');
+    });
+
+    it('Normalize path', async () => {
+      let result = normalizePath('D:\\test\\sub-dir\\file.js');
+      assert.equal(result, '/test/sub-dir/file.js');
+    });
   });
 
-  it('Relative path - POSIX', async () => {
-    let result = relatifyPath('\\test\\sub-dir\\file.js', '\\test\\sub-dir\\');
-    assert.equal(result, './file.js');
-  });
+  describe('posix', function () {
+    it('Relative path', async () => {
+      let result = relatifyPath('/test/sub-dir/file.js', '/test/sub-dir/');
+      assert.equal(result, './file.js');
+    });
 
-  it('Relative path - POSIX', async () => {
-    let result = relatifyPath('/test/sub-dir/file.js', '/test/sub-dir/');
-    assert.equal(result, './file.js');
-  });
-
-  it('Normalize path - WIN32', async () => {
-    let result = normalizePath('D:\\test\\sub-dir\\file.js');
-    assert.equal(result, '/test/sub-dir/file.js');
-  });
-
-  it('Normalize path - POSIX', async () => {
-    let result = normalizePath('\\test\\sub-dir\\file.js');
-    assert.equal(result, '/test/sub-dir/file.js');
-  });
-
-  it('Normalize path - POSIX', async () => {
-    let result = normalizePath('/test/sub-dir/file.js');
-    assert.equal(result, '/test/sub-dir/file.js');
+    it('Normalize path', async () => {
+      let result = normalizePath('/test/sub-dir/file.js');
+      assert.equal(result, '/test/sub-dir/file.js');
+    });
   });
 });
