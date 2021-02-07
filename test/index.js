163af07f33b4ed0ec0bb9059b587b80c263df8e8
Jasper De Moor <jasperdemoor@gmail.com>
2021-02-07 16:50:00 +0100

    remove segfault-handler for now...

--- test/index.js
+++ test/index.js
@@ -1,5 +1,3 @@
-require('segfault-handler');
-
 if (!process.env.BACKEND || process.env.BACKEND === 'node') {
   module.exports = require('../dist/node');
 } else if (process.env.BACKEND === 'wasm') {
