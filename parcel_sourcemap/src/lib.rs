d3d2d89693d775bfc0f0b97362227f9462e6e53b
Jasper De Moor <jasperdemoor@gmail.com>
2021-03-22 19:29:14 +0100

    allow adding mappings

--- parcel_sourcemap/src/lib.rs
+++ parcel_sourcemap/src/lib.rs
@@ -1,12 +1,33 @@
 pub mod mapping_line;
 
+use mapping_line::mapping::Mapping;
 use mapping_line::MappingLine;
+use std::collections::BTreeMap;
 
-struct SourceMap {
+pub struct SourceMap {
     _sources: Vec<String>,
     _sources_content: Vec<String>,
     _names: Vec<String>,
-    _mapping_lines: Vec<MappingLine>,
+    _mapping_lines: BTreeMap<i32, MappingLine>,
+}
+
+impl SourceMap {
+    pub fn new() -> Self {
+        Self {
+            _sources: Vec::new(),
+            _sources_content: Vec::new(),
+            _names: Vec::new(),
+            _mapping_lines: BTreeMap::new(),
+        }
+    }
+
+    pub fn add_mapping(&mut self, mapping: Mapping) {
+        let line = self
+            ._mapping_lines
+            .entry(mapping.generated.line)
+            .or_insert(MappingLine::new(mapping.generated.line));
+        line.add_mapping(mapping);
+    }
 }
 
 #[cfg(test)]
--- parcel_sourcemap/src/mapping_line.rs
+++ parcel_sourcemap/src/mapping_line.rs
@@ -2,8 +2,22 @@ use std::collections::BTreeMap;
 
 pub mod mapping;
 
-use mapping::{Mapping};
+use mapping::Mapping;
 
 pub struct MappingLine {
     _mappings: BTreeMap<i32, Mapping>,
+    pub line_number: i32,
+}
+
+impl MappingLine {
+    pub fn new(line_number: i32) -> Self {
+        Self {
+            line_number,
+            _mappings: BTreeMap::new(),
+        }
+    }
+
+    pub fn add_mapping(&mut self, mapping: Mapping) {
+        self._mappings.insert(mapping.generated.column, mapping);
+    }
 }
