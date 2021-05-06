57b16d892489d6f2887b6d3a01d72d3fdb1b9d29
Jasper De Moor <jasperdemoor@gmail.com>
2021-05-06 20:22:47 +0200

    improve artifacts move scrtipt

--- move-artifacts.js
+++ move-artifacts.js
@@ -4,10 +4,12 @@ const globby = require('globby');
 
 const ARTIFACTS_ROOT = path.join(__dirname, 'parcel_sourcemap_node/artifacts');
 
-let artifacts = globby.sync(path.join(ARTIFACTS_ROOT, '**'));
+let artifacts = globby.sync(path.join(ARTIFACTS_ROOT, '*/*.node'));
 if (!artifacts.length) {
   throw new Error('No artifacts found!');
 }
+
+console.log('Moving artifacts...');
 
 for (let artifact of artifacts) {
   let stat = fs.statSync(artifact);
@@ -15,19 +17,22 @@ for (let artifact of artifacts) {
     let filename = path.basename(artifact);
     fs.moveSync(artifact, path.join(ARTIFACTS_ROOT, filename));
     console.log('Moved:', filename);
-
-    let dirname = path.dirname(artifact);
-    if (fs.readdirSync(dirname).length === 0) {
-      fs.unlinkSync(dirname);
-      console.log('Removed:', dirname);
-    } else {
-      console.warn('Could not remove:', dirname, 'as it is not empty');
-    }
   }
 }
 
-console.log('=== Artifacts Moved ===');
-let artifactsContent = fs.readdirSync(ARTIFACTS_ROOT);
-for (let a of artifactsContent) {
-  console.log(a);
+console.log('Cleaning up artifacts folder...');
+
+let artifactsFolderContent = fs.readdirSync(ARTIFACTS_ROOT);
+for (let entry of artifactsFolderContent) {
+  let fullPath = path.join(ARTIFACTS_ROOT, entry);
+  if (!fullPath.endsWith('.node')) {
+    fs.removeSync(fullPath);
+    console.log('Removed:', fullPath);
+  }
+}
+
+console.log('=== ARTIFACTS ===');
+artifactsFolderContent = fs.readdirSync(ARTIFACTS_ROOT);
+for (let entry of artifactsFolderContent) {
+  console.log('-', entry);
 }
